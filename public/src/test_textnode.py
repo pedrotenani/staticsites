1a4fca80fe7ebca113f41b659736837929817c03
pedrotenani <pedrotenani1@gmail.com>
2025-03-12 11:09:10 -0300

    add split and extract methods

--- public/src/test_textnode.py
+++ public/src/test_textnode.py
@@ -24,6 +24,311 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD, "https://example.com")
         self.assertNotEqual(node, node2)
 
+    # Testes para a função split_nodes_delimiter
+    def test_split_nodes_delimiter_bold(self):
+        # Teste para delimitador de negrito
+        node = TextNode("This is **bold** text", TextType.TEXT)
+        nodes = TextNode.split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+    
+    def test_split_nodes_delimiter_multiple(self):
+        # Teste para múltiplos delimitadores no mesmo texto
+        node = TextNode("This is **bold** and **more bold** text", TextType.TEXT)
+        nodes = TextNode.split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("more bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+    
+    def test_split_nodes_delimiter_skip_non_text(self):
+        # Teste para verificar se nós que não são do tipo TEXT são ignorados
+        node1 = TextNode("This is **bold** text", TextType.TEXT)
+        node2 = TextNode("This is italic text", TextType.ITALIC)
+        nodes = TextNode.split_nodes_delimiter([node1, node2], "**", TextType.BOLD)
+        
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT),
+            TextNode("This is italic text", TextType.ITALIC)
+        ]
+        
+        self.assertEqual(nodes, expected)
+    
+    def test_split_nodes_delimiter_odd_delimiters(self):
+        # Teste para verificar se a função lança exceção com número ímpar de delimitadores
+        node = TextNode("This is **bold text", TextType.TEXT)
+        
+        with self.assertRaises(ValueError) as context:
+            TextNode.split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+        self.assertTrue("Odd number of '**' delimiters" in str(context.exception))
+    
+    def test_split_nodes_delimiter_no_delimiter(self):
+        # Teste para verificar se a função lança exceção quando não há delimitador
+        node = TextNode("This is plain text", TextType.TEXT)
+        
+        with self.assertRaises(ValueError) as context:
+            TextNode.split_nodes_delimiter([node], "**", TextType.BOLD)
+        
+        self.assertTrue("No '**' delimiter found" in str(context.exception))
+
+    def test_extract_markdown_images(self):
+        matches = TextNode.extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        # Teste para extrair múltiplas imagens
+        text = "Here are two images: ![first](https://example.com/img1.jpg) and ![second](https://example.com/img2.png)"
+        matches = TextNode.extract_markdown_images(text)
+        expected = [
+            ("first", "https://example.com/img1.jpg"),
+            ("second", "https://example.com/img2.png")
+        ]
+        self.assertListEqual(expected, matches)
+
+    def test_extract_markdown_images_no_images(self):
+        # Teste para verificar comportamento quando não há imagens
+        text = "This is just plain text with no images."
+        matches = TextNode.extract_markdown_images(text)
+        self.assertEqual(0, len(matches))
+
+    def test_extract_markdown_links(self):
+        # Teste básico para extrair links
+        text = "Visit [my website](https://example.com) for more information."
+        matches = TextNode.extract_markdown_links(text)
+        self.assertListEqual([("my website", "https://example.com")], matches)
+
+    def test_extract_markdown_links_with_images(self):
+        # Teste para verificar que links dentro de imagens não são capturados
+        text = "Here's a ![image](https://example.com/img.jpg) and a [link](https://example.com)"
+        matches = TextNode.extract_markdown_links(text)
+        self.assertListEqual([("link", "https://example.com")], matches)
+
+    # Testes para split_nodes_image
+    def test_split_nodes_image_basic(self):
+        # Teste básico para a função split_nodes_image
+        node = TextNode("This is an ![example](https://example.com/img.jpg) image", TextType.TEXT)
+        nodes = TextNode.split_nodes_image([node])
+        
+        expected = [
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("example", TextType.IMAGE, "https://example.com/img.jpg"),
+            TextNode(" image", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_image_multiple(self):
+        # Teste para múltiplas imagens
+        node = TextNode("![first](https://example.com/img1.jpg) and ![second](https://example.com/img2.png)", TextType.TEXT)
+        nodes = TextNode.split_nodes_image([node])
+        
+        expected = [
+            TextNode("first", TextType.IMAGE, "https://example.com/img1.jpg"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("second", TextType.IMAGE, "https://example.com/img2.png")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_image_no_images(self):
+        # Teste para texto sem imagens
+        node = TextNode("This is just plain text", TextType.TEXT)
+        nodes = TextNode.split_nodes_image([node])
+        
+        expected = [node]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_image_skip_non_text(self):
+        # Teste para verificar que nós não-TEXT são ignorados
+        node1 = TextNode("![image](https://example.com/img.jpg)", TextType.TEXT)
+        node2 = TextNode("This is bold", TextType.BOLD)
+        nodes = TextNode.split_nodes_image([node1, node2])
+        
+        expected = [
+            TextNode("image", TextType.IMAGE, "https://example.com/img.jpg"),
+            TextNode("This is bold", TextType.BOLD)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    # Testes adicionais para split_nodes_image
+    def test_split_nodes_image_empty_alt_text(self):
+        # Teste para imagem com texto alternativo vazio
+        node = TextNode("This is an image with empty alt text: ![](https://example.com/img.jpg)", TextType.TEXT)
+        nodes = TextNode.split_nodes_image([node])
+        
+        expected = [
+            TextNode("This is an image with empty alt text: ", TextType.TEXT),
+            TextNode("", TextType.IMAGE, "https://example.com/img.jpg")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_image_consecutive(self):
+        # Teste para imagens consecutivas sem texto entre elas
+        node = TextNode("![img1](https://example.com/img1.jpg)![img2](https://example.com/img2.jpg)", TextType.TEXT)
+        nodes = TextNode.split_nodes_image([node])
+        
+        expected = [
+            TextNode("img1", TextType.IMAGE, "https://example.com/img1.jpg"),
+            TextNode("img2", TextType.IMAGE, "https://example.com/img2.jpg")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    # Testes para split_nodes_link
+    def test_split_nodes_link_basic(self):
+        # Teste básico para a função split_nodes_link
+        node = TextNode("This is a [link](https://example.com) in text", TextType.TEXT)
+        nodes = TextNode.split_nodes_link([node])
+        
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com"),
+            TextNode(" in text", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_link_multiple(self):
+        # Teste para múltiplos links
+        node = TextNode("Visit [site1](https://example1.com) and [site2](https://example2.com)", TextType.TEXT)
+        nodes = TextNode.split_nodes_link([node])
+        
+        expected = [
+            TextNode("Visit ", TextType.TEXT),
+            TextNode("site1", TextType.LINK, "https://example1.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("site2", TextType.LINK, "https://example2.com")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_link_no_links(self):
+        # Teste para texto sem links
+        node = TextNode("This is just plain text", TextType.TEXT)
+        nodes = TextNode.split_nodes_link([node])
+        
+        expected = [node]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_link_skip_non_text(self):
+        # Teste para verificar que nós não-TEXT são ignorados
+        node1 = TextNode("[link](https://example.com)", TextType.TEXT)
+        node2 = TextNode("This is italic", TextType.ITALIC)
+        nodes = TextNode.split_nodes_link([node1, node2])
+        
+        expected = [
+            TextNode("link", TextType.LINK, "https://example.com"),
+            TextNode("This is italic", TextType.ITALIC)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    # Testes adicionais para split_nodes_link
+    def test_split_nodes_link_empty_text(self):
+        # Teste para link com texto vazio
+        node = TextNode("This is a link with empty text: [](https://example.com)", TextType.TEXT)
+        nodes = TextNode.split_nodes_link([node])
+        
+        expected = [
+            TextNode("This is a link with empty text: ", TextType.TEXT),
+            TextNode("", TextType.LINK, "https://example.com")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_split_nodes_link_consecutive(self):
+        # Teste para links consecutivos sem texto entre eles
+        node = TextNode("[link1](https://example1.com)[link2](https://example2.com)", TextType.TEXT)
+        nodes = TextNode.split_nodes_link([node])
+        
+        expected = [
+            TextNode("link1", TextType.LINK, "https://example1.com"),
+            TextNode("link2", TextType.LINK, "https://example2.com")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_text_to_textnodes_basic(self):
+        # Teste básico com diferentes tipos de formatação
+        text = "This is **bold** and *italic* text with `code`."
+        nodes = TextNode.text_to_textnodes(text)
+        
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text with ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(".", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_text_to_textnodes_complex(self):
+        # Teste com combinação de links, imagens e formatação
+        text = "Check this **important** [link](https://example.com) and ![image](https://example.com/img.jpg)"
+        nodes = TextNode.text_to_textnodes(text)
+        
+        expected = [
+            TextNode("Check this ", TextType.TEXT),
+            TextNode("important", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://example.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://example.com/img.jpg")
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_text_to_textnodes_mixed_delimiters(self):
+        # Teste com diferentes delimitadores misturados no mesmo texto
+        text = "This has **bold**, *italic*, and `code` all together"
+        nodes = TextNode.text_to_textnodes(text)
+        
+        expected = [
+            TextNode("This has ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(", ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(", and ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" all together", TextType.TEXT)
+        ]
+        
+        self.assertEqual(nodes, expected)
+
+    def test_text_to_textnodes_empty(self):
+        # Teste com texto vazio
+        text = ""
+        nodes = TextNode.text_to_textnodes(text)
+        
+        # Deve retornar um único nó de texto vazio
+        expected = [TextNode("", TextType.TEXT)]
+        
+        self.assertEqual(nodes, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class TextType(Enum):
     TEXT = "text"
@@ -21,5 +22,257 @@ class TextNode:
                 self.url == other.url)
     def __repr__(self):
         return f"TextNode('{str(self.text).upper()}', {str(self.text_type.value).upper()}, '{str(self.url).upper() if self.url else None}')"
+    
+    def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        """
+        Divide os nós de texto com base em um delimitador e atribui um tipo específico aos textos delimitados.
+        
+        Args:
+            old_nodes: Lista de TextNode a ser processada
+            delimiter: String que serve como delimitador (ex: "**", "_", "`")
+            text_type: O TextType a ser aplicado ao texto entre delimitadores
+        
+        Returns:
+            Uma nova lista de TextNode com os textos delimitados convertidos para o tipo especificado
+        
+        Raises:
+            ValueError: Se o markdown for inválido (número ímpar de delimitadores ou delimitador não encontrado)
+        """
+        new_nodes = []
+        delimiter_found = False
+        
+        for old_node in old_nodes:
+            # Só processamos nós do tipo TEXT
+            if old_node.text_type != TextType.TEXT:
+                new_nodes.append(old_node)
+                continue
+            
+            # Dividimos o texto pelos delimitadores
+            splits = old_node.text.split(delimiter)
+            
+            # Se não há delimitador no texto, mantemos o nó original
+            if len(splits) == 1:
+                new_nodes.append(old_node)
+                continue
+            
+            # Marcamos que encontramos pelo menos um delimitador
+            delimiter_found = True
+            
+            # Verificamos se temos um número ímpar de delimitadores (markdown inválido)
+            if len(splits) % 2 == 0:
+                raise ValueError(f"Invalid markdown: Odd number of '{delimiter}' delimiters")
+            
+            # Processamos os splits para criar novos nós
+            for i in range(len(splits)):
+                # Se o split não está vazio, adicionamos como nó de texto
+                if splits[i]:
+                    # Se é um índice par, é texto normal
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(splits[i], TextType.TEXT))
+                    # Se é um índice ímpar, é texto delimitado
+                    else:
+                        new_nodes.append(TextNode(splits[i], text_type))
+        
+        # Se não encontramos nenhum delimitador em nenhum nó, lançamos uma exceção
+        if not delimiter_found:
+            raise ValueError(f"Invalid markdown: No '{delimiter}' delimiter found")
+        
+        return new_nodes
+    
+    @staticmethod
+    def extract_markdown_images(text):
+        """
+        Extrai marcadores de imagens no formato Markdown ![alt text](url) de um texto
+        e retorna uma lista de tuplas (alt_text, url).
+        
+        Args:
+            text: String contendo o texto a ser analisado
+        
+        Returns:
+            Uma lista de tuplas, onde cada tupla contém (texto_alternativo, url_da_imagem)
+        
+        Exemplo:
+            Para o texto "Veja esta ![imagem](https://example.com/img.jpg) e ![outra](https://example.com/img2.png)"
+            Retorna: [("imagem", "https://example.com/img.jpg"), ("outra", "https://example.com/img2.png")]
+        """
+        # Padrão regex para encontrar imagens no formato ![alt text](url)
+        pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+        
+        # Encontra todas as ocorrências do padrão no texto
+        matches = re.findall(pattern, text)
+        
+        # Retorna a lista de tuplas (alt_text, url)
+        return matches
+    
+    @staticmethod
+    def extract_markdown_links(text):
+        """
+        Extrai marcadores de links no formato Markdown [texto do link](url) de um texto
+        e retorna uma lista de tuplas (texto_do_link, url).
+        
+        Returns:
+            Uma lista de tuplas, onde cada tupla contém (texto_do_link, url)
+        
+        Exemplo:
+            Para o texto "Visite [Google](https://google.com) e [GitHub](https://github.com)"
+            Retorna: [("Google", "https://google.com"), ("GitHub", "https://github.com")]
+        """
+        # Padrão regex para encontrar links no formato [texto do link](url)
+        # Exclui links de imagem que começam com !
+        pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+        
+        # Encontra todas as ocorrências do padrão no texto
+        matches = re.findall(pattern, text)
+        
+        # Retorna a lista de tuplas (texto_do_link, url)
+        return matches
+    
+    @staticmethod
+    def split_nodes_image(old_nodes):
+        """
+        Processa nós de texto para identificar marcadores de imagem no formato Markdown ![alt](url)
+        e convertê-los em nós do tipo IMAGE.
+        
+        Args:
+            old_nodes: Lista de TextNode a ser processada
+        
+        Returns:
+            Uma nova lista de TextNode com os marcadores de imagem convertidos para nós do tipo IMAGE
+        """
+        new_nodes = []
+        
+        for old_node in old_nodes:
+            # Só processamos nós do tipo TEXT
+            if old_node.text_type != TextType.TEXT:
+                new_nodes.append(old_node)
+                continue
+            
+            # Extraímos as imagens do texto
+            images = TextNode.extract_markdown_images(old_node.text)
+            
+            # Se não há imagens, mantemos o nó original
+            if not images:
+                new_nodes.append(old_node)
+                continue
+            
+            # Processamos o texto para substituir as imagens por nós apropriados
+            remaining_text = old_node.text
+            
+            for alt_text, url in images:
+                # Encontramos a posição da imagem no texto
+                image_marker = f"![{alt_text}]({url})"
+                parts = remaining_text.split(image_marker, 1)
+                
+                # Adicionamos o texto antes da imagem, se houver
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                
+                # Adicionamos a imagem como um nó IMAGE
+                new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                
+                # Atualizamos o texto restante
+                remaining_text = parts[1] if len(parts) > 1 else ""
+            
+            # Adicionamos o texto restante, se houver
+            if remaining_text:
+                new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+        
+        return new_nodes
+    
+    @staticmethod
+    def split_nodes_link(old_nodes):
+        """
+        Processa nós de texto para identificar marcadores de link no formato Markdown [text](url)
+        e convertê-los em nós do tipo LINK.
+        
+        Args:
+            old_nodes: Lista de TextNode a ser processada
+        
+        Returns:
+            Uma nova lista de TextNode com os marcadores de link convertidos para nós do tipo LINK
+        """
+        new_nodes = []
+        
+        for old_node in old_nodes:
+            # Só processamos nós do tipo TEXT
+            if old_node.text_type != TextType.TEXT:
+                new_nodes.append(old_node)
+                continue
+            
+            # Extraímos os links do texto
+            links = TextNode.extract_markdown_links(old_node.text)
+            
+            # Se não há links, mantemos o nó original
+            if not links:
+                new_nodes.append(old_node)
+                continue
+            
+            # Processamos o texto para substituir os links por nós apropriados
+            remaining_text = old_node.text
+            
+            for link_text, url in links:
+                # Encontramos a posição do link no texto
+                link_marker = f"[{link_text}]({url})"
+                parts = remaining_text.split(link_marker, 1)
+                
+                # Adicionamos o texto antes do link, se houver
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                
+                # Adicionamos o link como um nó LINK
+                new_nodes.append(TextNode(link_text, TextType.LINK, url))
+                
+                # Atualizamos o texto restante
+                remaining_text = parts[1] if len(parts) > 1 else ""
+            
+            # Adicionamos o texto restante, se houver
+            if remaining_text:
+                new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+        
+        return new_nodes
+    
+    @staticmethod
+    def text_to_textnodes(text):
+        """
+        Transforma um texto em Markdown em uma lista de TextNode,
+        processando delimitadores, links e imagens.
+        
+        Args:
+            text: String contendo o texto em formato Markdown
+        
+        Returns:
+            Uma lista de TextNode representando o texto processado
+        """
+        # Começamos com um único nó de texto
+        nodes = [TextNode(text, TextType.TEXT)]
+        
+        # Processamos os delimitadores para texto em negrito
+        try:
+            nodes = TextNode.split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        except ValueError:
+            # Se não houver delimitadores de negrito, continuamos com os nós atuais
+            pass
+        
+        # Processamos os delimitadores para texto em itálico
+        try:
+            nodes = TextNode.split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        except ValueError:
+            # Se não houver delimitadores de itálico, continuamos com os nós atuais
+            pass
+        
+        # Processamos os delimitadores para código
+        try:
+            nodes = TextNode.split_nodes_delimiter(nodes, "`", TextType.CODE)
+        except ValueError:
+            # Se não houver delimitadores de código, continuamos com os nós atuais
+            pass
+        
+        # Processamos imagens
+        nodes = TextNode.split_nodes_image(nodes)
+        
+        # Processamos links
+        nodes = TextNode.split_nodes_link(nodes)
+        
+        return nodes
     
     
